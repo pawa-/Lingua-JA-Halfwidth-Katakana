4105868c214e8858e7dd96745af0191d1d4dd383
pawa <pawa@dojikko.com>
2012-06-14 23:04:35 +0900

    initial commit

---
+++ lib/Lingua/JA/Halfwidth/Katakana.pm
@@ -0,0 +1,32 @@
+package Lingua::JA::Halfwidth::Katakana;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Lingua::JA::Halfwidth::Katakana -
+
+=head1 SYNOPSIS
+
+  use Lingua::JA::Halfwidth::Katakana;
+
+=head1 DESCRIPTION
+
+Lingua::JA::Halfwidth::Katakana is
+
+=head1 AUTHOR
+
+pawa- E<lt>pawa@dojikko.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
